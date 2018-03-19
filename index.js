896ed2ae5a89f9c2e90bc4709c7e092c4a7301e1
Tobias Meister <meister.tobias21@gmail.com>
2018-03-19 23:33:08 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+const Config = require('config.json');
+
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+
+function randomBetween(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let newMembers = new Map();
+
+Client.on('ready', () => {
+	Client.guilds.forEach((guild, id) => newMembers.set(id, new Set()));
+});
+
+Client.on('guildCreate', guild => {
+	newMembers.set(guild.id, new Set());
+});
+
+Client.on('guildDelete', guild => {
+	newMembers.delete(guild.id);
+});
+
+Client.on('guildMemberAdd', member => {
+	let guild = member.guild;
+
+	newMembers.get(guild.id).add(member.id);
+
+	let channel = guild.channels.find('name', 'general');
+	channel.send(`Hi <@${member.id}>, what's your name?`);
+});
+
+Client.on('guildMemberRemove', member => {
+	let guild = member.guild;
+
+	if (newMembers.get(guild.id).has(member.id)) {
+		newMembers.get(guild.id).delete(member.id);
+	}
+});
+
+const approving = [
+	'Of course!',
+	'Sure.',
+	'Ahh,',
+	'Very well.',
+	'Nice!'
+];
+
+const countering = [
+	'Hmm...',
+	'Well...',
+	'Yeah, about that...',
+	'Actually...'
+];
+
+function randomEntry(array) {
+	return array[randomBetween(0, array.length - 1)];
+}
+
+Client.on('message', async (msg) => {
+	if (msg.author === Client.user) return;
+	if (!(msg.channel instanceof Discord.TextChannel)) return;
+
+	const parts = msg.content.split(' ');
+	const cmd = parts[0];
+	const args = parts.slice(1);
+
+	const guild = msg.guild;
+	const member = await guild.fetchMember(msg.author);
+
+	try {
+		let isNewUser = newMembers.get(guild.id).has(member.id);
+
+		if (cmd === '--nick' || isNewUser) {
+			let name = isNewUser
+					? cmd + ' ' + args.join(' ')
+					: args.join(' ');
+
+			await member.setNickname(name + ' | ' + msg.author.username);
+			if (isNewUser) {
+				msg.channel.send(randomEntry(approving) + ` Welcome to the server, ${name} :smile:`);
+			} else {
+				msg.channel.send(randomEntry(countering) + ' still a shit name, tho ' + Client.emojis.find('425405967036055553'));
+			}
+		}
+
+	} catch (error) {
+		msg.channel.send(`[Error] Couldn't process command ${msg.content} | see console`);
+		console.error(error);
+	} finally {
+		newMembers.get(guild.id).delete(member.id);
+	}
+});
+
+Client.on('error', console.error);
+
+Client.login(Config.token);
